f1a076dfe708907ab09089e9579ffe6deb7730e3
Favorwynne <favourfreshboy@gmail.com>
2023-12-22 16:45:20 -0800

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -2,29 +2,26 @@
 
 /**
  * set_pair - mallocs a key/value pair to the hash table.
+ * @node: a pointer to the hash table array node.
  * @key: the key, a string that cannot be empty.
  * @value: the value associated with the key, can be an empty string.
  *
- * Return: pointer to the new node.
+ * Return: 1 on success, 0 on error.
  */
-hash_node_t *set_pair(const char *key, const char *value)
+int set_pair(hash_node_t **node, const char *key, const char *value)
 {
-	hash_node_t *node = malloc(sizeof(hash_node_t));
-
-	if (node == NULL)
+	*node = malloc(sizeof(hash_node_t));
+	if (*node == NULL)
 		return (0);
-	printf("hi from set_pair\n");
-	node->key = malloc(strlen(key) + 1);
-	if (node->key == NULL)
+	(*node)->key = malloc(strlen(key) + 1);
+	if ((*node)->key == NULL)
 		return (0);
-	node->value = malloc(strlen(value) + 1);
-	if (node->value == NULL)
+	(*node)->value = malloc(strlen(value) + 1);
+	if ((*node)->value == NULL)
 		return (0);
-	strcpy(node->key, key);
-	strcpy(node->value, value);
-	if (strcmp(node->key, key) == 0)
-		printf("set_pair succeeded\n");
-	return (node);
+	strcpy((*node)->key, key);
+	strcpy((*node)->value, value);
+	return (1);
 }
 
 /**
@@ -46,12 +43,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node = ht->array[index];
 	if (node == NULL)
 	{
-		printf("calling set_pair before while loop\n");
-		node = set_pair(key, value);
+		if (set_pair(&node, key, value) == 0)
+			return (0);
 		node->next = NULL;
-		ht->array[index] = node;
-		if (strcmp(ht->array[index]->key, key) == 0)
-			printf("assigning key and value worked\n");
 		return (1);
 	}
 	while (node != NULL)
@@ -71,7 +65,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	if (node == NULL)
 	{
-		node = set_pair(key, value);
+		if (set_pair(&node, key, value) == 0)
+			return (0);
 		node->next = ht->array[index];
 		ht->array[index] = node;
 		return (1);
